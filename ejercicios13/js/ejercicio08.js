8093add8121764a8bb9d536a5ab6caf7ddab388f
Cjesus10 <118759299+Cjesus10@users.noreply.github.com>
2022-11-22 10:52:17 -0500

    Update ejercicio08.js

--- ejercicios13/js/ejercicio08.js
+++ ejercicios13/js/ejercicio08.js
@@ -3,4 +3,4 @@ let monto = prompt("Ingresar un monto");
 let a = monto*30/100; 
 let b = monto*20/100;
 let c = monto*50/100;
-document.write(`A = ${a} </br> B = ${b} </br> C = ${c} `)
+document.write(`Socio A = ${a} </br> Socio B = ${b} </br> Socio C = ${c} `)
